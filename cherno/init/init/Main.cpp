a0fe2ae48bd99523684171ce63cf6e0f86f2e6c9
Mahanth <Mahanth-Maha@users.noreply.github.com>
2022-04-29 09:41:19 +0530

    power function

--- cherno/init/init/Main.cpp
+++ cherno/init/init/Main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+double mypow(double x,double y) {
+	double sol = 1;
+	for (double i = 0; i < y; i = i+1) sol *= x;
+	return sol;
+}
+
 int main() {
+	double s = mypow(2, 10);
 	std::cout << "Hello There !" << std::endl ;
+	std::cout << s << std::endl;
 	std::cin.get();
 }
