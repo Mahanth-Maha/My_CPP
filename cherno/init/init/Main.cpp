e2e96943106e4e716493aa6980b065334b58a364
Mahanth <Mahanth-Maha@users.noreply.github.com>
2022-04-30 13:34:20 +0530

    Memory - Debug

--- cherno/init/init/Main.cpp
+++ cherno/init/init/Main.cpp
@@ -1,14 +1,16 @@
 #include <iostream>
 
-double mypow(double x,double y) {
-	double sol = 1;
-	for (double i = 0; i < y; i = i+1) sol *= x;
+int mypow(int x,int y) {
+	int sol = 1;
+	for (int i = 0; i < y; i = i+1) sol *= x;
 	return sol;
 }
 
 int main() {
-	double s = mypow(2, 10);
+	int s = mypow(2, 10);
 	std::cout << "Hello There !" << std::endl ;
-	std::cout << s << std::endl;
+	std::cout << "2 ^ 10 = " << s << std::endl;
+	std::cout << "The memory sizes of\nchar\t\t" << sizeof(char) << "\nshort\t\t" << sizeof(short) << "\nint\t\t" << sizeof(int);
+	std::cout << "\nlong\t\t" << sizeof(long) << "\nlong long\t" << sizeof(long long ) << "\nfloat\t\t" << sizeof(float) << "\ndouble\t\t" << sizeof(double);
 	std::cin.get();
 }
