f1fcae0325c3b2a9e66262c122ddae6cef75086d
myalla <yallamahanth2002@gmail.com>
2023-07-12 09:52:35 +0530

    ignore exe

---
+++ Test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+#define LOG(x) std::cout << x << std::endl;
+
+using namespace std;
+
+int main()
+{
+    int  T,n;
+    cin >> T;
+    while(T--){
+        cin >> n;
+        cout << n << endl;
+    }
+
+    return 0;
+}
